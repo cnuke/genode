a808022f7e0b808e528a814b7d498f88f48723a3
Josef Söntgen <jws@sysret.de>
2015-12-19 10:33:43 +0100

    libports: use sftp example as libssh test --- REMOVE ME LATER

--- repos/libports/src/test/libports/libssh/target.mk
+++ repos/libports/src/test/libports/libssh/target.mk
@@ -1,7 +1,8 @@
-TARGET = test-libssh
-LIBS   = base libcrypto libssh
-SRC_CC = main.cc
-
-vpath main.cc $(PRG_DIR)/..
+TARGET := test-libssh
+LIBS   := libc libcrypto libssh posix
+SRC_C  := authentication.c \
+          connect_ssh.c \
+          knownhosts.c \
+          main.c
 
 CC_CXX_WARN_STRICT =
