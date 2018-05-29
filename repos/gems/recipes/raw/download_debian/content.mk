184b68df4e236ad256308745803b1ef95202f411
Josef Söntgen <josef.soentgen@genode-labs.com>
2018-05-29 17:10:42 +0200

    gems: add pkg for Debian 13.1.0 VM assets

---
+++ repos/gems/recipes/raw/download_debian/content.mk
@@ -0,0 +1,10 @@
+content: init.config assets.tar
+
+FILE_PATH := $(REP_DIR)/recipes/raw/download_debian
+
+init.config:
+	cp $(FILE_PATH)/$@ $@
+
+assets.tar:
+	tar --mtime='2022-04-29 00:00Z' -rf $@ -C $(FILE_PATH) ./machine.vbox6
+	tar --mtime='2022-04-29 00:00Z' -rf $@ -C $(FILE_PATH) ./machine.vdi
