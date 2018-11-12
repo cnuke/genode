47ad064ab1d897ca66ec05aa24bd6d11bcadcf11
Josef Söntgen <josef.soentgen@genode-labs.com>
2018-11-12 16:43:39 +0100

    fixup vfs_oss (buffer)

--- repos/gems/src/lib/vfs/oss/vfs_oss.cc
+++ repos/gems/src/lib/vfs/oss/vfs_oss.cc
@@ -15,6 +15,7 @@
 #include <audio_out_session/connection.h>
 #include <base/registry.h>
 #include <base/signal.h>
+#include <gems/magic_ring_buffer.h>
 #include <util/xml_generator.h>
 #include <vfs/device_file_system.h>
 #include <vfs/readonly_value_file_system.h>
@@ -50,20 +51,24 @@ class Vfs_oss::File_system : public Device_file_system
 				Audio(Audio const &);
 				Audio &operator = (Audio const &);
 
+				Genode::Magic_ring_buffer<float> _left_buffer;
+				Genode::Magic_ring_buffer<float> _right_buffer;
+
+				bool _started { false };
+
 			public:
 
 				enum { CHANNELS = 2, };
 				const char *_channel_names[CHANNELS] = { "front left", "front right" };
 
 				Genode::Constructible<Audio_out::Connection> _out[CHANNELS];
-				Audio_out::Packet *_out_packet[CHANNELS] { };
 
 				Audio(Genode::Env &env, Label &)
+				: _left_buffer(env, 1u << 20), _right_buffer(env, 1u << 20)
 				{
 					for (int i = 0; i < CHANNELS; i++) {
 						try {
 							_out[i].construct(env, _channel_names[i], false, false);
-							_out_packet[i] = nullptr;
 						} catch (...) {
 							Genode::error("could not create Audio_out channel ", i);
 							throw;
@@ -73,34 +78,53 @@ class Vfs_oss::File_system : public Device_file_system
 
 				void pause()
 				{
-					for (int i = 0; i < CHANNELS; i++) {
-						_out[i]->stop();
-						_out_packet[i] = nullptr;
-					}
+					for (int i = 0; i < CHANNELS; i++) { _out[i]->stop(); }
+					_started = false;
 				}
 
 				bool write(char const *buf, file_size buf_size, file_size &out_size)
 				{
-					if (!_out_packet[0]) {
-						for (int i = 0; i < CHANNELS; i++) { _out[i]->start(); }
-						_out_packet[0] = _out[0]->stream()->next();
-						_out_packet[0] = _out[0]->stream()->next(_out_packet[0]);
-					}
+					Genode::size_t const samples = Genode::min(_left_buffer.write_avail(), buf_size/2);
 
-					unsigned const pos = _out[0]->stream()->packet_position(_out_packet[0]);
-					_out_packet[1] = _out[1]->stream()->get(pos);
+					float *dest[2] = { _left_buffer.write_addr(), _right_buffer.write_addr() };
 
-					for (Genode::size_t sample = 0; sample < Audio_out::PERIOD; sample++) {
+					for (Genode::size_t i = 0; i < samples/2; i++) {
 						for (int c = 0; c < CHANNELS; c++) {
-							float *p = _out_packet[c]->content();
-							p[sample] = (float)(((Genode::int16_t*)buf)[sample * CHANNELS + c]) / 32768;
+							float *p = dest[c];
+							Genode::int16_t const v = ((Genode::int16_t const*)buf)[i * CHANNELS + c];
+							p[i] = ((float)v) / 32768.0f;
 						}
 					}
 
-					for (int c = 0; c < CHANNELS; c++) { _out[c]->submit(_out_packet[c]); }
+					_left_buffer.fill(samples/2);
+
+					while (_left_buffer.read_avail() > Audio_out::PERIOD) {
+
+						if (!_started) {
+							_started = true;
+
+							_out[0]->start();
+							_out[1]->start();
+						}
+
+						Audio_out::Packet *lp = _out[0]->stream()->next();
+						unsigned const pos = _out[0]->stream()->packet_position(lp);
+						Audio_out::Packet *rp = _out[1]->stream()->get(pos);
+
+						float const *src[CHANNELS] = { _left_buffer.read_addr(),
+						                               _right_buffer.read_addr() };
+
+						Genode::memcpy(lp->content(), src[0], Audio_out::PERIOD * 4);
+						_left_buffer.drain(Audio_out::PERIOD);
+						Genode::memcpy(rp->content(), src[1], Audio_out::PERIOD * 4);
+						_right_buffer.drain(Audio_out::PERIOD);
+
+						_out[0]->submit(lp);
+						_out[1]->submit(rp);
+					}
 
 					/* XXX */
-					out_size = buf_size;
+					out_size = samples * 2;
 					return true;
 				}
 		};
@@ -113,8 +137,8 @@ class Vfs_oss::File_system : public Device_file_system
 			Oss_vfs_file_handle(Directory_service    &ds,
 			                    File_io_service      &fs,
 			                    Genode::Allocator    &alloc,
-			                    Audio &audio)
-			: Device_vfs_handle(ds, fs, alloc, 0), _audio(audio) { }
+			                    Audio &audio, int flags)
+			: Device_vfs_handle(ds, fs, alloc, flags), _audio(audio) { }
 
 			/* not supported */
 			Read_result read(char *, file_size, file_size &) {
@@ -132,7 +156,7 @@ class Vfs_oss::File_system : public Device_file_system
 		typedef Genode::Registry<Registered_handle>     Handle_registry;
 		Handle_registry _handle_registry { };
 
-		using Config = Genode::String<256>;
+		using Config = Genode::String<4096>;
 		static Config _dir_config(Genode::Xml_node node)
 		{
 			char buf[Config::capacity()] { };
@@ -150,6 +174,9 @@ class Vfs_oss::File_system : public Device_file_system
 				xml.node("readonly_value", [&] { xml.attribute("name", "queued"); });
 				xml.node("readonly_value", [&] { xml.attribute("name", "sample_rate"); });
 				xml.node("readonly_value", [&] { xml.attribute("name", "sample_size"); });
+				xml.node("readonly_value", [&] { xml.attribute("name", "frag_size"); });
+				xml.node("readonly_value", [&] { xml.attribute("name", "frag_avail"); });
+				xml.node("readonly_value", [&] { xml.attribute("name", "frag_used"); });
 			});
 			return Config(Genode::Cstring(buf));
 		}
@@ -161,6 +188,10 @@ class Vfs_oss::File_system : public Device_file_system
 		Readonly_value_file_system<unsigned> _sample_rate_fs { _env, "sample_rate", Audio_out::SAMPLE_RATE };
 		Readonly_value_file_system<unsigned> _sample_size_fs { _env, "sample_size", sizeof (Genode::int16_t) };
 
+		Readonly_value_file_system<unsigned> _ofrag_size_fs  { _env, "frag_size", 8192 }; // XXX calculate
+		Readonly_value_file_system<unsigned> _ofrag_avail_fs { _env, "frag_avail",  32 };
+		Readonly_value_file_system<unsigned> _ofrag_used_fs  { _env, "frag_used",    0 };
+
 		Vfs::File_system *create(Vfs::Env &, Genode::Xml_node node) override
 		{
 			if (node.has_type(Readonly_value_file_system<unsigned>::type_name())) {
@@ -170,9 +201,11 @@ class Vfs_oss::File_system : public Device_file_system
 				     : _queued_fs.matches(node)      ? &_queued_fs
 				     : _sample_rate_fs.matches(node) ? &_sample_rate_fs
 				     : _sample_size_fs.matches(node) ? &_sample_size_fs
+				     : _ofrag_size_fs.matches(node)  ? &_ofrag_size_fs
+				     : _ofrag_avail_fs.matches(node) ? &_ofrag_avail_fs
+				     : _ofrag_used_fs.matches(node)  ? &_ofrag_used_fs
 				     : nullptr;
 			}
-
 			return nullptr;
 		}
 
@@ -185,7 +218,7 @@ class Vfs_oss::File_system : public Device_file_system
 			_name(config.attribute_value("name", Name(name()))),
 			_env(env), _io_handler(env.io_handler()),
 			_watch_handler(env.watch_handler()),
-			_audio(env.env(), _label)
+			_audio(_env.env(), _label)
 		{
 			Device_file_system::construct(env, Genode::Xml_node(_dir_config(config).string()), *this);
 		}
@@ -197,19 +230,19 @@ class Vfs_oss::File_system : public Device_file_system
 		 ** Directory service interface **
 		 *********************************/
 
-		Open_result open(char const  *path, unsigned,
+		Open_result open(char const  *path, unsigned flags,
 		                 Vfs_handle **out_handle,
 		                 Allocator   &alloc) override
 		{
 			try {
 				if (_device_file(path)) {
 					*out_handle = new (alloc)
-						Registered_handle(_handle_registry, *this, *this, alloc, _audio);
+						Registered_handle(_handle_registry, *this, *this, alloc, _audio, flags);
 					return OPEN_OK;
 				} else
 
 				if (_device_dir_file(path)) {
-					return Device_file_system::open(path, 0, out_handle, alloc);
+					return Device_file_system::open(path, flags, out_handle, alloc);
 				}
 			}
 			catch (Genode::Out_of_ram)  { return OPEN_ERR_OUT_OF_RAM; }
