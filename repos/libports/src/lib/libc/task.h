93f8e72a73bbdac5e38d6fc6148f259746c50cb9
Josef Söntgen <josef.soentgen@genode-labs.com>
2019-08-12 18:10:14 +0200

    libc: add VFS watch handle accessor
    Issue #3450.

--- repos/libports/src/lib/libc/task.h
+++ repos/libports/src/lib/libc/task.h
@@ -23,6 +23,7 @@
 
 #include <base/duration.h>
 #include <util/xml_node.h>
+#include <vfs/vfs_handle.h>
 
 namespace Libc {
 
@@ -50,6 +51,10 @@ namespace Libc {
 	Genode::uint64_t suspend(Suspend_functor &, Genode::uint64_t timeout_ms = 0);
 
 	void dispatch_pending_io_signals();
+
+	/**
+	 */
+	Vfs::Vfs_watch_handle *watch(char const *path);
 
 	/**
 	 * Get time since startup in ms
