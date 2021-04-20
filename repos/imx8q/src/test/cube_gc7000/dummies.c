e30ddae664a84632e60c3509b32d2ceb79e17a4d
Josef Söntgen <josef.soentgen@genode-labs.com>
2021-04-20 18:08:04 +0200

    imx8q: add (old) cube_gc7000 test
    THis includes the old etnaviv gpu tests repo. This is done to have
    some more meaningful tests while the new Mesa port is coming up.

---
+++ repos/imx8q/src/test/cube_gc7000/dummies.c
@@ -0,0 +1,14 @@
+int fb_open(struct etna_device *conn, int num, struct fb_info **out_p)
+{
+	return -1;
+}
+
+int fb_set_buffer(struct fb_info *fb, int buffer)
+{
+	return -1;
+}
+
+int fb_close(struct fb_info *fb)
+{
+	return -1;
+}
