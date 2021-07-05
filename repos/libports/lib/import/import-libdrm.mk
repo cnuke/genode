b3d0fff41c79515256c7e0b49e3701bb2f83473b
Josef Söntgen <josef.soentgen@genode-labs.com>
2021-07-05 18:14:28 +0200

    fixup libdrm-105

--- repos/libports/lib/import/import-libdrm.mk
+++ repos/libports/lib/import/import-libdrm.mk
@@ -1,3 +1,8 @@
-include $(call select_from_repositories,/lib/import/import-libdrm.inc)
+ifeq ($(CONTRIB_DIR),)
+DRM_SRC_DIR := $(call select_from_ports,libdrm)/src/lib/libdrm
+else
+DRM_SRC_DIR = $(call select_from_ports,libdrm)/src/lib/libdrm
+endif
 
+INC_DIR += $(DRM_SRC_DIR) $(addprefix $(DRM_SRC_DIR)/,include/drm include)
 INC_DIR += $(DRM_SRC_DIR) $(addprefix $(DRM_SRC_DIR)/,etnaviv)
