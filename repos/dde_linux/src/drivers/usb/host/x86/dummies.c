b95d247a3697fe271350a397838d7b8945e64bee
Josef Söntgen <josef.soentgen@genode-labs.com>
2022-01-10 17:51:59 +0100

    dde_linux: import USB host driver from imx to x86 -- WIP
    Again, like a64 to x86_64 it is not supposed to work yet.

---
+++ repos/dde_linux/src/drivers/usb/host/x86/dummies.c
@@ -0,0 +1,14 @@
+/*
+ * \brief  Dummy definitions of Linux Kernel functions - handled manually
+ * \author Josef Soentgen
+ * \date   2022-01-10
+ */
+
+/*
+ * Copyright (C) 2021 Genode Labs GmbH
+ *
+ * This file is distributed under the terms of the GNU General Public License
+ * version 2.
+ */
+
+#include <lx_emul.h>
---
+++ repos/dde_linux/src/drivers/usb/host/x86/target.mk
@@ -0,0 +1,21 @@
+REQUIRES := x86
+
+TARGET  := x86_usb_host_drv
+LIBS    := base x86_lx_emul
+INC_DIR := $(PRG_DIR)
+SRC_CC  += main.cc
+SRC_CC  += time.cc
+SRC_C   += dummies.c
+SRC_C   += lx_emul.c
+SRC_C   += usb.c
+SRC_C   += $(notdir $(wildcard $(PRG_DIR)/generated_dummies.c))
+
+CC_OPT_drivers/usb/host/xhci-trace += -I$(LX_CONTRIB_DIR)/drivers/usb/host
+
+#
+# Genode C-API backends
+#
+
+SRC_CC  += genode_c_api/usb.cc
+
+vpath genode_c_api/usb.cc $(subst /genode_c_api,,$(call select_from_repositories,src/lib/genode_c_api))
