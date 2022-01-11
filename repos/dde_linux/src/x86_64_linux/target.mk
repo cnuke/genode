638e2f2bd6e27d94ab1713d953ed4c2ade1f6c82
Josef Söntgen <josef.soentgen@genode-labs.com>
2022-01-11 14:30:24 +0100

    x86_64_linux: build minimal kernel w/ USB support

--- repos/dde_linux/src/x86_64_linux/target.mk
+++ repos/dde_linux/src/x86_64_linux/target.mk
@@ -1,7 +1,8 @@
 TARGET   := x86_64_linux
 REQUIRES := x86_64
 
-CUSTOM_TARGET_DEPS := kernel_build.phony
+#CUSTOM_TARGET_DEPS := kernel_build.phony
+CUSTOM_TARGET_DEPS := run_kernel.phony
 
 LX_DIR := $(call select_from_ports,x86_linux)/src/linux
 PWD    := $(shell pwd)
@@ -16,7 +17,7 @@ LX_MK_ARGS = ARCH=x86_64 CROSS_COMPILE=$(CROSS_DEV_PREFIX)
 #
 
 # define 'LX_ENABLE' and 'LX_DISABLE'
-include $(REP_DIR)/src/x86_linux/target.inc
+include $(REP_DIR)/src/x86_64_linux/target.inc
 
 # filter for make output of kernel build system
 BUILD_OUTPUT_FILTER = 2>&1 | sed "s/^/      [Linux]  /"
@@ -24,8 +25,8 @@ BUILD_OUTPUT_FILTER = 2>&1 | sed "s/^/      [Linux]  /"
 kernel_config.tag:
 	$(MSG_CONFIG)Linux
 	$(VERBOSE)$(MAKE) -C $(LX_DIR) O=$(PWD) $(LX_MK_ARGS) tinyconfig $(BUILD_OUTPUT_FILTER)
-	$(VERBOSE)$(LX_DIR)/scripts/config $(addprefix --enable ,$(LX_ENABLE))
-	$(VERBOSE)$(LX_DIR)/scripts/config $(addprefix --disable ,$(LX_DISABLE))
+	$(VERBOSE)$(LX_DIR)/scripts/config --file $(PWD)/.config $(addprefix --enable ,$(LX_ENABLE))
+	$(VERBOSE)$(LX_DIR)/scripts/config --file $(PWD)/.config $(addprefix --disable ,$(LX_DISABLE))
 	$(VERBOSE)$(MAKE) $(LX_MK_ARGS) olddefconfig $(BUILD_OUTPUT_FILTER)
 	$(VERBOSE)touch $@
 
@@ -34,4 +35,10 @@ kernel_config.tag: $(MAKEFILE_LIST)
 
 kernel_build.phony: kernel_config.tag
 	$(MSG_BUILD)Linux
-	$(VERBOSE)$(MAKE) $(LX_MK_ARGS) dtbs Image $(BUILD_OUTPUT_FILTER)
+	$(VERBOSE)$(MAKE) $(LX_MK_ARGS) bzImage $(BUILD_OUTPUT_FILTER)
+
+run_kernel.phony: kernel_build.phony
+	$(MSG_BUILD)Linux
+	$(VERBOSE)qemu-system-x86_64 -nographic -serial mon:stdio \
+		-device nec-usb-xhci,id=xhci -device usb-ehci,id=ehci \
+		-kernel $(PWD)/arch/x86_64/boot/bzImage -append "console=ttyS0"
