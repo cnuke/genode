e6b38ced70f5e59711896ac08b066cfbb1b699ac
Josef Söntgen <josef.soentgen@genode-labs.com>
2022-01-31 17:47:45 +0100

    dde_linux: x86_32_lx_emul

---
+++ repos/dde_linux/src/include/spec/x86_32/lx_emul/shadow/asm/string_32.h
@@ -0,0 +1,7 @@
+/**
+ * \brief  Shadow copy of linux/string_32.h
+ * \author Josef Soentgen
+ * \date   2022-01-31
+ */
+
+#pragma once
---
+++ repos/dde_linux/src/include/spec/x86_32/lx_emul/shadow/linux/swapops.h
@@ -0,0 +1,7 @@
+/**
+ * \brief  Shadow copy of linux/swapops.h
+ * \author Josef Soentgen
+ * \date   2022-01-31
+ */
+
+#pragma once
