f86b7b49bdd481ec7ad823290451d51cb171a51f
Josef Söntgen <josef.soentgen@genode-labs.com>
2022-02-01 14:34:21 +0100

    dde_linux: add new USB HC based on Linux 5.14.21

---
+++ repos/dde_linux/src/drivers/usb/host/pc/lx_emul/pci_fixups.h
@@ -0,0 +1,6 @@
+#pragma once
+
+static const char * lx_emul_pci_final_fixups[] = {
+	"__pci_fixup_final_quirk_usb_early_handoff",
+	"END_OF_PCI_FIXUPS"
+};
---
+++ repos/dde_linux/src/drivers/usb/host/pc/spec/x86_64/target.mk
@@ -0,0 +1,3 @@
+include $(REP_DIR)/src/drivers/usb/host/pc/target.inc
+
+REQUIRES += 64bit
