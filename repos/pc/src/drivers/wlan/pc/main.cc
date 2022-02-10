6285bc1118fa4549109c8bb77c97e36720e1cf7b
Josef Söntgen <josef.soentgen@genode-labs.com>
2022-02-10 18:09:17 +0100

    pc: initial wlan driver skeleton
    The driver falls in line with the previous 'wifi_drv' component where
    the ported wireless LAN stack and device driver is encapsulated in a
    library. This library in return is used by the 'Libc::Component'
    providing the necessary environment for the 'wpa_supplicant'.
    
    In constrast to the old driver a 'wlan' VFS plugin is in charge of
    initalizing the 'Lx_kit::Env' prior to executing any static
    constructors.

---
+++ repos/pc/src/drivers/wlan/pc/main.cc
@@ -0,0 +1,160 @@
+/*
+ * \brief  Startup Wifi driver
+ * \author Josef Soentgen
+ * \date   2014-03-03
+ */
+
+/*
+ * Copyright (C) 2014-2017 Genode Labs GmbH
+ *
+ * This file is distributed under the terms of the GNU General Public License
+ * version 2.
+ */
+
+/* Genode includes */
+#include <libc/component.h>
+#include <base/attached_rom_dataspace.h>
+#include <base/log.h>
+#include <base/sleep.h>
+#include <os/reporter.h>
+#include <timer_session/connection.h>
+#include <util/xml_node.h>
+
+/* local includes */
+#include <util.h>
+#include <wpa.h>
+#include <frontend.h>
+
+
+using namespace Genode;
+
+
+static Wifi::Frontend *_wifi_frontend = nullptr;
+
+
+/**
+ * Notify front end about command processing
+ *
+ * Called by the CTRL interface after wpa_supplicant has processed
+ * the command.
+ */
+void wifi_block_for_processing(void)
+{
+	if (!_wifi_frontend) {
+		warning("frontend not available, dropping notification");
+		return;
+	}
+
+	Genode::error(__func__, ":", __LINE__);
+	/*
+	 * Next time we block as long as the front end has not finished
+	 * handling our previous request
+	 */
+	_wifi_frontend->block_for_processing();
+
+	/* XXX hack to trick poll() into returning faster */
+	wpa_ctrl_set_fd();
+	Genode::error(__func__, ":", __LINE__);
+}
+
+
+void wifi_notify_cmd_result(void)
+{
+	if (!_wifi_frontend) {
+		warning("frontend not available, dropping notification");
+		return;
+	}
+
+	Genode::error(__func__, ":", __LINE__);
+	Signal_transmitter(_wifi_frontend->result_sigh()).submit();
+}
+
+
+/**
+ * Notify front end about triggered event
+ *
+ * Called by the CTRL interface whenever wpa_supplicant has triggered
+ * a event.
+ */
+void wifi_notify_event(void)
+{
+	if (!_wifi_frontend) {
+		Genode::warning("frontend not available, dropping notification");
+		return;
+	}
+
+	Signal_transmitter(_wifi_frontend->event_sigh()).submit();
+}
+
+
+/* exported by wifi.lib.so */
+extern void wifi_init(Genode::Env&,
+                      Genode::Blockade&,
+                      bool,
+                      Genode::Signal_context_capability);
+
+struct Main
+{
+	Env  &env;
+
+	Constructible<Wpa_thread>     _wpa;
+	Constructible<Wifi::Frontend> _frontend;
+
+	Blockade _wpa_startup_blockade { };
+
+	Main(Genode::Env &env) : env(env)
+	{
+		// _frontend.construct(env);
+		// _wifi_frontend = &*_frontend;
+
+		_wpa.construct(env, _wpa_startup_blockade);
+
+		/*
+		 * Forcefully disable 11n but for convenience the attribute is used the
+		 * other way araound.
+		 */
+		bool const disable_11n = false;//!_frontend->use_11n();
+		wifi_init(env, _wpa_startup_blockade, disable_11n,
+		          Genode::Signal_context_capability());
+		          // _frontend->rfkill_sigh());
+	}
+};
+
+static Main *_main;
+
+
+/**
+ * Return shared-memory message buffer
+ *
+ * It is used by the wpa_supplicant CTRL interface.
+ */
+void *wifi_get_buffer(void)
+{
+	// void *ptr = _wifi_frontend ? &_wifi_frontend->msg_buffer() : nullptr;
+	// Genode::error(__func__, ":", __LINE__, ": ptr: ", ptr);
+
+	Genode::error(__func__, ":", __LINE__, ": _wifi_frontend: ", _wifi_frontend);
+	if (_wifi_frontend)
+		return &_wifi_frontend->msg_buffer();
+
+	Libc::with_libc([&] () {
+
+		if (_main->_frontend.constructed())
+			return;
+
+		_main->_frontend.construct(_main->env);
+		_wifi_frontend = &*_main->_frontend;
+	});
+
+	Genode::error(__func__, ":", __LINE__, ": _wifi_frontend: ", _wifi_frontend);
+	return &_wifi_frontend->msg_buffer();
+}
+
+
+void Libc::Component::construct(Libc::Env &env)
+{
+	Libc::with_libc([&] () {
+		static Main server(env);
+		_main = &server;
+	});
+}
