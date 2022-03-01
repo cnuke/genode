e0a1db18e674af057b0aab6370dcbee02fb6bf49
Josef Söntgen <josef.soentgen@genode-labs.com>
2022-03-01 13:37:01 +0100

    pc_wlan: allow for using static wlan library

--- repos/pc/lib/mk/spec/x86_64/wlan.mk
+++ repos/pc/lib/mk/spec/x86_64/wlan.mk
@@ -1,3 +1,5 @@
 include $(REP_DIR)/lib/mk/wlan.inc
 
+SHARED_LIB := yes
+
 REQUIRES += 64bit
--- repos/pc/src/drivers/wlan/pc/target.mk
+++ repos/pc/src/drivers/wlan/pc/target.mk
@@ -1,6 +1,8 @@
 TARGET  := pc_wlan_drv
 SRC_CC  := main.cc wpa.cc
-LIBS    := base wlan iwl_firmware
+LIBS    := base iwl_firmware
+LIBS    += wlan
+#LIBS    += wlan_static
 LIBS    += libc
 LIBS    += wpa_supplicant
 LIBS    += libcrypto libssl wpa_driver_nl80211
