52f4dcc4918077a2a3b95ed367a5176f0cc5b639
Alexander Boettcher <alexander.boettcher@genode-labs.com>
2022-10-13 13:35:34 +0200

    sculpt_manager: up to 30% for the log window - TMP

--- repos/gems/src/app/sculpt_manager/main.cc
+++ repos/gems/src/app/sculpt_manager/main.cc
@@ -1700,8 +1700,6 @@ void Sculpt::Main::_update_window_layout(Xml_node const &decorator_margins,
 
 	Decorator_margins const margins { decorator_margins };
 
-	unsigned const log_min_w = 400;
-
 	typedef String<128> Label;
 	Label const
 		inspect_label          ("runtime -> leitzentrale -> inspect"),
@@ -1734,6 +1732,9 @@ void Sculpt::Main::_update_window_layout(Xml_node const &decorator_margins,
 
 	/* area reserved for the panel */
 	Rect const panel(Point(0, 0), Area(mode.area.w(), panel_height));
+
+	/* log area - at least 400p , max 30% of the available width */
+	unsigned const log_min_w = max (400u, mode.area.w() * 30 / 100);
 
 	/* available space on the right of the menu */
 	Rect avail(Point(0, panel.h()),
