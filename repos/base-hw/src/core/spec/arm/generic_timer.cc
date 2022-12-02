2622944c97f62b1f950cbdedb1d1077e8d1a7b0e
Josef Söntgen <josef.soentgen@genode-labs.com>
2022-12-02 15:54:40 +0100

    Revert "T base-hw: mitigate unstable generic timer on A64"
    This reverts commit e03f19dc9c28a914140e15341e149708c8dffa75.

--- repos/base-hw/src/core/spec/arm/generic_timer.cc
+++ repos/base-hw/src/core/spec/arm/generic_timer.cc
@@ -43,16 +43,11 @@ void Timer::_start_one_shot(time_t const ticks)
 
 time_t Timer::_duration() const
 {
-	time_t v, u;
+	time_t v;
 
-	v = Cpu::Cntpct::read();
-	u = Cpu::Cntpct::read();
-	long long diff = ((long long)u) - ((long long)v);
-	if (diff < 0) {
-		time_t w = Cpu::Cntpct::read();
-		Genode::raw("timer unstable: ", Genode::Hex(v), " ", Genode::Hex(u), " ", Genode::Hex(w));
-		v = w;
-	}
+	do {
+		v = Cpu::Cntpct::read();
+	} while (((v + 1) & 0x7ff) <= 1);
 
 	return v - _time;
 }
