519fd4123e8e35be396801e8c02382d2b5e4d868
Josef Söntgen <josef.soentgen@genode-labs.com>
2023-03-10 16:53:35 +0100

    wifi: defer supplicant start-up
    The SDIO connected wireless device on the PinePhone is not available
    on start-up, which leads to the supplicant failing to initalize
    'wlan0'. Normally the supplicant would be used in a way that handling
    devices that appear at run-time happens gracefully.
    
    Rather than supporting this behavior we defer the start-up of the
    supplicant until the device could be openend successfully for now.

--- repos/pc/src/lib/wifi/uplink.c
+++ repos/pc/src/lib/wifi/uplink.c
@@ -226,6 +226,7 @@ static int uplink_netdev_event(struct notifier_block *this,
 	return NOTIFY_DONE;
 }
 
+extern void wakeup_wpa(void);
 
 static int user_task_function(void *arg)
 {
@@ -246,7 +247,8 @@ static int user_task_function(void *arg)
 				continue;
 
 			/* enable link sensing, repeated calls are handled by testing IFF_UP */
-			dev_open(dev, 0);
+			if (dev_open(dev, 0) == 0)
+				wakeup_wpa();
 
 			/* install rx handler once */
 			if (!netdev_is_rx_handler_busy(dev))
