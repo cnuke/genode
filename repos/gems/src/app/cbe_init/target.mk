93cf444afec422ea48abaa232e4684f94e57a4f2
Martin Stein <martin.stein@genode-labs.com>
2023-03-23 15:52:06 +0100

    gems: new c++ cbe as library

--- repos/gems/src/app/cbe_init/target.mk
+++ repos/gems/src/app/cbe_init/target.mk
@@ -1,26 +1,12 @@
-REQUIRES += x86_64
-
 TARGET := cbe_init
 
-SRC_CC := main.cc
-SRC_CC += \
-          block_allocator.cc \
-          block_io.cc \
-          crypto.cc \
-          ft_initializer.cc \
-          module.cc \
-          sha256_4k_hash.cc \
-          sb_initializer.cc \
-          trust_anchor.cc \
-          vbd_initializer.cc \
-          vfs_utilities.cc
+SRC_CC += main.cc
 
-INC_DIR += $(REP_DIR)/src/app/cbe_tester
 INC_DIR += $(PRG_DIR)
-LIBS    += base vfs libcrypto
 
-vpath main.cc $(PRG_DIR)
-vpath    %.cc $(REP_DIR)/src/app/cbe_tester
+LIBS += base
+LIBS += cbe
 
-CONFIG_XSD = config.xsd
+CONFIG_XSD := config.xsd
+
 CC_CXX_WARN_STRICT_CONVERSION =
--- repos/gems/src/app/cbe_tester/target.mk
+++ repos/gems/src/app/cbe_tester/target.mk
@@ -1,27 +1,10 @@
-REQUIRES := x86_64
+TARGET := cbe_tester
 
-TARGET  := cbe_tester
-SRC_CC  += main.cc
-SRC_CC  += crypto.cc
-SRC_CC  += request_pool.cc
-SRC_CC  += sha256_4k_hash.cc
-SRC_CC  += trust_anchor.cc
-SRC_CC  += block_io.cc
-SRC_CC  += meta_tree.cc
-SRC_CC  += virtual_block_device.cc
-SRC_CC  += superblock_control.cc
-SRC_CC  += free_tree.cc
-SRC_CC  += vfs_utilities.cc
-SRC_CC  += module.cc
-SRC_CC  += block_allocator.cc
-SRC_CC  += vbd_initializer.cc
-SRC_CC  += ft_initializer.cc
-SRC_CC  += sb_initializer.cc
+SRC_CC += main.cc
 
-INC_DIR := $(PRG_DIR)
+INC_DIR += $(PRG_DIR)
 
 LIBS += base
-LIBS += vfs
-LIBS += libcrypto
+LIBS += cbe
 
 CC_CXX_WARN_STRICT_CONVERSION =
