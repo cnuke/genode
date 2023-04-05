f1ad73865af50943d4be2557db62aca7d359a75e
Norman Feske <norman.feske@genode-labs.com>
2023-04-05 16:19:47 +0200

    DEBUG sculpt: verbose wifi

--- repos/gems/src/app/sculpt_manager/network.h
+++ repos/gems/src/app/sculpt_manager/network.h
@@ -158,8 +158,8 @@ struct Sculpt::Network : Network_dialog::Action
 				xml.attribute("scan_interval", 10U);
 				xml.attribute("use_11n", false);
 
-				xml.attribute("verbose_state", false);
-				xml.attribute("verbose",       false);
+				xml.attribute("verbose_state", true);
+				xml.attribute("verbose",       true);
 
 				xml.node("network", [&]() {
 					xml.attribute("ssid", ap.ssid);
@@ -193,8 +193,8 @@ struct Sculpt::Network : Network_dialog::Action
 			xml.attribute("scan_interval", 10U);
 			xml.attribute("use_11n", false);
 
-			xml.attribute("verbose_state", false);
-			xml.attribute("verbose",       false);
+			xml.attribute("verbose_state", true);
+			xml.attribute("verbose",       true);
 
 			xml.node("network", [&]() {
 				/* generate attributes to ease subsequent manual tweaking */
