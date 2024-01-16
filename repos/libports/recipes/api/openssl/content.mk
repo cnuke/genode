b568e3d6ab97653116803cd2a951b85bc3e15d61
Josef Söntgen <josef.soentgen@genode-labs.com>
2024-01-16 16:47:56 +0100

    api/openssl: add pkg-config files
    Issue genodelabs/goa#81.

--- repos/libports/recipes/api/openssl/content.mk
+++ repos/libports/recipes/api/openssl/content.mk
@@ -24,8 +24,26 @@ src/lib/openssl:
 	cp -r $(REP_DIR)/src/lib/openssl/crypto $@/
 	cp -r $(REP_DIR)/src/lib/openssl/spec $@/
 
-content: LICENSE
+content: LICENSE libcrypto.pc libssl.pc openssl.pc
 
 LICENSE:
 	cp $(PORT_DIR)/src/lib/openssl/LICENSE $@
 
+libcrypto.pc:
+	echo 'Name: OpenSSL-libcrypto' > $@
+	echo 'Description: OpenSSL cryptography library' >> $@
+	echo 'Version: 1.1.1t' >> $@
+	echo 'Libs: -l:libcrypto.lib.so' >> $@
+
+libssl.pc:
+	echo 'Name: OpenSSL-libssl' > $@
+	echo 'Description: Secure Sockets Layer and cryptography libraries' >> $@
+	echo 'Version: 1.1.1t' >> $@
+	echo 'Requires.private: libcrypto' >> $@
+	echo 'Libs: -l:libssl.lib.so' >> $@
+
+openssl.pc:
+	echo 'Name: OpenSSL' > $@
+	echo 'Description: Secure Sockets Layer and cryptography libraries and tools' >> $@
+	echo 'Version: 1.1.1t' >> $@
+	echo 'Requires: libssl libcrypto' >> $@
