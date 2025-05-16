2a35c6eab594edc7a88884cd20af6d1f6a959e39
Josef Söntgen <josef.soentgen@genode-labs.com>
2025-05-16 16:06:35 +0200

    driver/acpi: avoid using Xml_node::sub_node
    Issue #5245.

--- repos/os/src/driver/acpi/smbios_table_reporter.cc
+++ repos/os/src/driver/acpi/smbios_table_reporter.cc
@@ -147,10 +147,11 @@ Smbios_table_reporter::Smbios_table_reporter(Env       &env,
 
 	addr_t efi_sys_tab_phy = 0;
 	try {
-		Attached_rom_dataspace info(env, "platform_info");
-		Xml_node xml(info.local_addr<char>(), info.size());
-		Xml_node acpi_node = xml.sub_node("efi-system-table");
-		efi_sys_tab_phy = acpi_node.attribute_value("address", 0UL);
+		Attached_rom_dataspace(env, "platform_info")
+			.xml().with_optional_sub_node("efi-system-table",
+				[&] (Xml_node const &acpi_node) {
+					efi_sys_tab_phy =
+						acpi_node.attribute_value("address", 0UL); });
 	} catch (...) { }
 
 	if (!efi_sys_tab_phy) {
