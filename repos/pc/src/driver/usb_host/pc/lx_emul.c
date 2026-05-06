9c471af9ad1aaa7cf38074e07c6118a34f5274d1
Josef Söntgen <josef.soentgen@genode-labs.com>
2026-05-06 11:10:46 +0200

    fixup 'dde_linux: support MSI(X)' (pc_usb_host)

--- repos/pc/src/driver/usb_host/pc/lx_emul.c
+++ repos/pc/src/driver/usb_host/pc/lx_emul.c
@@ -13,6 +13,7 @@
 
 #include <lx_emul.h>
 #include <linux/pci.h>
+#include <linux/list.h>
 
 void __iomem * pci_ioremap_bar(struct pci_dev * pdev, int bar)
 {
@@ -87,29 +88,6 @@ int pci_write_config_word(const struct pci_dev * dev,int where,u16 val)
 }
 
 
-int pci_alloc_irq_vectors_affinity(struct pci_dev *dev, unsigned int min_vecs,
-                                   unsigned int max_vecs, unsigned int flags,
-                                   struct irq_affinity *aff_desc)
-{
-	if ((flags & PCI_IRQ_INTX) && min_vecs == 1 && dev->irq)
-		return 1;
-	return -ENOSPC;
-}
-
-
-int pci_irq_vector(struct pci_dev *dev, unsigned int nr)
-{
-	if (WARN_ON_ONCE(nr > 0))
-		return -EINVAL;
-	return dev->irq;
-}
-
-
-void pci_free_irq_vectors(struct pci_dev *dev)
-{
-}
-
-
 #include <linux/uaccess.h>
 
 unsigned long _copy_from_user(void * to,const void __user * from,unsigned long n)
@@ -128,12 +106,86 @@ unsigned long _copy_to_user(void __user * to,const void * from,unsigned long n)
 }
 
 
-#include <linux/pci.h>
+int pci_alloc_irq_vectors_affinity(struct pci_dev *dev, unsigned int min_vecs,
+                                   unsigned int max_vecs, unsigned int flags,
+                                   struct irq_affinity *aff_desc)
+{
+	if ((flags & PCI_IRQ_INTX) && min_vecs == 1 && dev->irq)
+		return 1;
+	return -ENOSPC;
+}
+
+
+int pci_irq_vector(struct pci_dev *dev, unsigned int nr)
+{
+	if (WARN_ON_ONCE(nr > 0))
+		return -EINVAL;
+	return dev->irq;
+}
+
+
+void pci_free_irq_vectors(struct pci_dev *dev) { }
+
+
+struct pci_dev_msix_table_entry
+{
+	struct list_head node;
+
+	struct pci_dev    *dev;
+	struct msix_entry *table;
+};
+
+
+static LIST_HEAD(pci_dev_msix_table_entry_list);
+
 
 int pci_alloc_irq_vectors(struct pci_dev * dev, unsigned int min_vecs,
                           unsigned int max_vecs,unsigned int flags)
 {
+	int nvecs = -ENOSPC;
+
+	if (flags & PCI_IRQ_MSIX) {
+
+		struct pci_dev_msix_table_entry *entry = NULL;
+		/* lookup or create table for pci_dev */
+		{
+			struct pci_dev_msix_table_entry *pos;
+
+			list_for_each_entry(pos, &pci_dev_msix_table_entry_list, node) {
+				if (pos->dev == dev)
+					entry = pos;
+			}
+
+			if (!entry) {
+				entry = kzalloc(sizeof(*entry), GFP_KERNEL);
+				if (!entry)
+					return -ENOMEM;
+
+				entry->dev = dev;
+				list_add(&entry->node, &pci_dev_msix_table_entry_list);
+			}
+		}
+
+		/* for the moment just wipe any existing entries */
+		kfree(entry->table);
+		entry->table = kmalloc_array(max_vecs, sizeof(struct msix_entry),
+		                             GFP_KERNEL);
+		if (!entry->table)
+			return -ENOMEM;
+
+		nvecs = pci_enable_msix_range(dev, entry->table, min_vecs, max_vecs);
+	}
+	if (nvecs > 0)
+		return nvecs;
+
+	if (flags & PCI_IRQ_MSI)
+		nvecs = pci_enable_msi(dev);
+	/* 0 is good and 1 vec supported */
+	if (nvecs == 0)
+		return 1;
+
 	if ((flags & PCI_IRQ_INTX) && min_vecs == 1 && dev->irq)
 		return 1;
+
 	return -ENOSPC;
 }
